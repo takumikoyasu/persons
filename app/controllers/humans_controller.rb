20cb199a690fb36a7644a7a8d39eb33f69551349
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-06-14 14:51:16 +0900

    名前とエピソードを登録可能

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,10 +1,21 @@
 class HumansController < ApplicationController
+  
   def index
+    @humans = Human.all
   end
 
   def new
+    @human = Human.new
   end
 
   def create
+    Human.create(human_params)
+    
   end
+
+  private
+  def human_params
+    params.require(:human).permit(:name, :episode)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'humans#index'
-  resources :humans, except: [:index]
+  resources :humans, only: [:new, :create]
   
 end
